01c6665854c9307ac10954daeed59dcdb592fdf7
jesus estaba <jeec.estaba@gmail.com>
2016-02-14 21:39:32 -0430

    mejoras cart & working pay

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -9,6 +9,58 @@
 		</p>
 	</div>
 	<div class="col-md-3 col-md-offset-3">
-		<img height="50" src="{{asset('images/logos/Flat-Social-Network-Icons.png')}}" alt="SocialIcons">
+		{!!Html::style('css/style_techandall.css')!!}
+		<link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+		
+		<style type="text/css">
+		.social-links a{
+			color: white !important;
+			border-color: white !important;
+		}
+		#social3{
+			margin-top: 5px;
+		}
+		.facebook a:hover{
+			border-color:#3b5a9b !important;
+		}
+.googleplus a:hover{
+	border-color:#f63d26 !important;
+}
+.instagram a:hover{
+	border-color:#507ea4 !important;
+}
+.twitter a:hover{
+	border-color:#2baae1 !important;
+}
+		</style>
+		
+		<div id="social3">
+        <ul class="social-links clearfix">
+            <li class="facebook">
+                <a href="http://facebook.com" title="Facebook" target="_blank">
+                    <i class="icon-facebook">
+                    </i>
+                </a>
+            </li>
+            <li class="googleplus">
+                <a href="https://plus.google.com" title="Google+" target="_blank">
+                    <i class="icon-google-plus">
+                    </i>
+                </a>
+            </li>
+            <li class="instagram">
+                <a href="http://instagram.com/" title="Instagram" target="_blank">
+                    <i class="icon-instagram">
+                    </i>
+                </a>
+            </li>
+            <li class="twitter">
+                <a href="http://www.twitter.com/" title="Twitter" target="_blank">
+                    <i class="icon-twitter">
+                    </i>
+                </a>
+            </li>      
+        </ul>
+    </div>
 	</div>
 </footer>
