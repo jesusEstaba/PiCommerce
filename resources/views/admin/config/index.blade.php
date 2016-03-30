bd5a92b9379e91bf74d729755eafdf75666da508
jesus estaba <jeec.estaba@gmail.com>
2016-03-30 09:27:19 -0430

    Mejoras Admin 3.8

--- resources/views/admin/config/index.blade.php
+++ resources/views/admin/config/index.blade.php
@@ -20,6 +20,8 @@
 		@else
 			<span class="btn btn-default glyphicon glyphicon-off off"></span>
 		@endif
+
+		<span class="glyphicon closed_edit glyphicon-pencil btn btn-default"></span>
 	</h3>
 <div class="divisor">
 	
@@ -27,6 +29,30 @@
 		{{$config->message_close}}
 	</p>
 		</div>
+
+<hr>
+				<div class="box-header">
+					<h3>Footer
+						<span class="glyphicon footer_edit glyphicon-pencil btn btn-default"></span>
+					</h3>
+				</div>
+				<div class="box-body">
+					{!! $config->footer !!}
+				</div>
+<hr>
+				<div class="box-header">
+					<h3>Change Logo
+						<span class="glyphicon logo_edit glyphicon-pencil btn btn-default"></span>
+					</h3>
+				</div>
+				<div class="box-body">
+					<img class="img-responsive" src="{{asset('images/logos/'.$config->logo) }}">
+				</div>
+
+
+
+
+
 	</div>
 		
 
@@ -47,12 +73,14 @@
 		</div>
 
 
-		
+	
 	
 
 
 
 	</div>
+
+
 </div>
 
 <div class="box">
@@ -185,7 +213,8 @@
       </div>
       <div class="modal-body">
 	
-	<div class="input-group">
+	<div class="form-group">
+		<label>Day:</label>
 		<select class="form-control" name="day">
 			<option value="">Day</option>
 			<option value="1">Monday</option>
@@ -197,11 +226,11 @@
 			<option value="7">Sunday</option>
 		</select>
 	</div>
-      	<div class="input-group">
+      	<div class="form-group">
       		<label>Open: </label>
 	      <input type="time" class="form-control" name="open" placeholder="Open" autocomplete="off">
 	    </div>
-	    <div class="input-group">
+	    <div class="form-group">
 	    	<label>Close:</label>
 	      <input type="time" class="form-control" name="close" placeholder="Close" autocomplete="off">
 	    </div>    
@@ -224,7 +253,8 @@
       </div>
       <div class="modal-body">
 	
-	<div class="input-group">
+	<div class="form-group">
+		<label>Social Networks:</label>
 		<select class="form-control" name="social-net">
 			<option value="">Social Network</option>
 			<option value="1">Facebook</option>
@@ -233,8 +263,9 @@
 			<option value="4">Google+</option>
 		</select>
 	</div>
-      	<div class="input-group">
-	      <input type="text" class="form-control" name="url" placeholder="Url" autocomplete="off">
+      	<div class="form-group">
+      		<label>Url:</label>
+	      <input type="text" class="form-control" name="url" placeholder="Social Network URL" autocomplete="off">
 	    </div>  
         
       </div>
@@ -245,6 +276,93 @@
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
+
+
+
+
+<div id="myModalClosed" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Edit Closed</h4>
+      </div>
+      <div class="modal-body">
+	
+      	<div class="form-group">
+      		<label>Message:</label>
+	      <textarea class="form-control" name="closed_message" placeholder="message when the site is closed"></textarea>
+	    </div>  
+        
+      </div>
+      <div class="modal-footer">
+      	<button type="button" class="btn btn-primary save-closed" data-dismiss="modal">Save</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+<div id="myModalFooter" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Edit Footer</h4>
+      </div>
+      <div class="modal-body">
+	
+      	<div class="form-group">
+      		<label>Message Footer:</label>
+	      <textarea class="form-control" name="footer_html" placeholder="html content">
+	      	{{$config->footer}}
+	      </textarea>
+	    </div>  
+        
+      </div>
+      <div class="modal-footer">
+      	<button type="button" class="btn btn-primary save-footer" data-dismiss="modal">Save</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
+<div id="myModalLogo" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Edit Logo</h4>
+      </div>
+      <div class="modal-body">
+	
+	<div class="form-group">
+		<label>Logo Image:</label>
+		<input type="file" name="logo_img" id="logo_img" class="form-control"></input>
+	</div> 
+        
+      </div>
+      <div class="modal-footer">
+      	<button type="button" class="btn btn-primary save-logo" data-dismiss="modal">Save</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -275,6 +393,100 @@ color:green;
 
 
 
+$('.footer_edit').click(function(){
+	$('#myModalFooter').modal();
+});
+$('.logo_edit').click(function(){
+	$('#myModalLogo').modal();
+});
+
+
+$('.closed_edit').click(function(){
+	$('#myModalClosed').modal();
+});
+
+
+$('.save-closed').click(function(){
+	if( $("[name=closed_message]").val() )
+	{
+		$.ajax({
+				url: 'config/5',
+				type: 'PUT',
+				dataType: 'json',
+				headers:{'X-CSRF-TOKEN' : $('[name=_token]').val()},
+				data: {
+					message:$("[name=closed_message]").val()
+				},
+			})
+			.done(function(data)
+			{
+				document.location.reload(true);
+			});
+	}
+	else
+		alert("Field Empty");
+
+});
+
+
+$('.save-footer').click(function(){
+	if( $("[name=footer_html]").val() )
+	{
+		$.ajax({
+				url: 'config/4',
+				type: 'PUT',
+				dataType: 'json',
+				headers:{'X-CSRF-TOKEN' : $('[name=_token]').val()},
+				data: {
+					footer:$("[name=footer_html]").val()
+				},
+			})
+			.done(function(data)
+			{
+					document.location.reload(true);
+			});
+	}
+	else
+		alert("Field Empty");
+
+});
+
+$('.save-logo').click(function(){
+
+	if( $("[name=logo_img]").val() )
+	{
+
+		var img_ele = document.getElementById('logo_img');
+		var file_img = img_ele.files[0];
+
+		var data = new FormData();
+
+		data.append('logo', file_img);
+
+		$.ajax({
+				type: 'POST',
+				dataType: 'json',
+				headers:{'X-CSRF-TOKEN' : $('[name=_token]').val()},
+				data:data,
+				contentType:false,
+				data:data,
+				processData:false,
+				cache:false
+			})
+			.done(function(data) {
+				document.location.reload(true);
+			});
+
+	}	
+	else
+		alert("Field Empty");
+});
+
+
+
+
+
+
 	$('.save.social').click(function(){
 		
 		if( $("[name=social-net]").val() )
@@ -295,6 +507,7 @@ color:green;
 				{
 					$("[name=social-net]").val("");
 					$("[name=url]").val("");
+					document.location.reload(true);
 				}
 			});
 
@@ -323,6 +536,7 @@ color:green;
 					$("[name=day]").val("");
 					$('[name=open]').val("");
 					$('[name=close]').val("");
+					document.location.reload(true);
 				}
 			});
 
