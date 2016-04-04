97af42955cd4ca39c2759c2319089ccbffb35462
jesus estaba <jeec.estaba@gmail.com>
2016-04-04 14:02:50 -0430

    pruebas correo

--- app/Http/Controllers/SendMailCTRL.php
+++ app/Http/Controllers/SendMailCTRL.php
@@ -11,24 +11,32 @@ use Mail;
 
 class SendMailCTRL extends Controller
 {
-    
-public function send_mail(){
-	$variables_correo = ['me_le_ponen'=>'gas del bueno'];
+    /**
+     * [send_mail description]
+     * @return [type] [description]
+     */
+    public function send_mail()
+    {
+    	       
+        $variables_correo = ['me_le_ponen'=>'gas del bueno'];
 
-			Mail::send('template_mail.prueba', $variables_correo, function($msj)
-			{
-            	$msj->subject('Order');
-            	$msj->from(env('MAIL_ADDRESS'), env('MAIL_NAME'));
-            	$msj->to('jeec.estaba@gmail.com');
-            });
-            
-            if(count(Mail::failures()))
-            {
-            	$errors = 'Failed to send password reset email, please try again.';
-            }
-    if( !isset($errors) )
-    		$errors = "todo correcto <a href='cart'>Cart</a>";
-		
-		return $errors;
-}
+        $correos = ['jeec.estaba@gmail.com', 'estaba_jesus@hotmail.com'];
+
+        Mail::send('template_mail.prueba', $variables_correo, function($msj)
+        {
+            $msj->subject('Order');
+            $msj->from(env('MAIL_ADDRESS'), env('MAIL_NAME'));
+            $msj->to($correos);
+        });
+
+        if(count(Mail::failures()))
+        {
+            $errors = 'Failed to send password reset email, please try again.';
+        }
+        
+        if( !isset($errors) )
+            $errors = "todo correcto <a href='cart'>Cart</a>";
+
+        return $errors;
+    }
 }
