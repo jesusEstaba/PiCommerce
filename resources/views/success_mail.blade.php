22be3697ce17060a2c69bacfeb16f2195bf8f099
jesus estaba <jeec.estaba@gmail.com>
2016-04-15 12:19:17 -0430

    Delete Confirmation Email

--- resources/views/success_mail.blade.php
+++ resources/views/success_mail.blade.php
@@ -7,8 +7,13 @@
 		<div class="col-xs-12">
 			<img style="margin: auto;display: block;" width="160" class="img-responsive" src="{{asset('images/logos/email-icon.png')}}">
 			<h3 class="text-center" style="margin-top: 1.5em;">
-				Register Success! now check your email and find the confirmation email.
+				Register Success! Now you can go to your email and find your user data.
 			</h3>
+			<br>
+			<br>
+			<p class="text-center">
+				<a href="{{url('login')}}" class="btn btn-success">Go to Login</a>
+			</p>
 			<br>
 		</div>
 	</div>
