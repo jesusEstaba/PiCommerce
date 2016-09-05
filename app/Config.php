261d38d76d7613e2b09f90027a0dc0eb08bca217
Jesus Estaba <jeec.estaba@gmail.com>
2016-09-05 00:05:24 -0400

    Nuevo Env, Modelo Config y migracion que quedaron pendientes

---
+++ app/Config.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pizza;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Config extends Model
+{
+    protected $table = 'config';
+
+    public static function message ($property) 
+    {
+    	$config = static::where('Cfg_Descript', $property)->first();
+
+        return ($config) ? $config->Cfg_Message : '';
+    }
+
+    public static function value1 ($property) 
+    {
+    	$config = static::where('Cfg_Descript', $property)->first();
+
+        return ($config) ? $config->Cfg_Value1 : 0;
+    }
+
+}
